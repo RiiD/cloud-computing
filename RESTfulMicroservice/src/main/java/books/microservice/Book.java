5d552973a7f318793689be532ce8d7f31302220d
DELL <DELL@192.168.1.15>
2019-03-11 20:52:51 +0200

    setup

---
+++ RESTfulMicroservice/src/main/java/books/microservice/Book.java
@@ -0,0 +1,64 @@
+package books.microservice;
+
+public class Book {
+
+	private String ISBN;
+	private String title;
+	private String[] authors;
+	private String publisher;
+	private int publishedYear;
+	private int rating;
+	
+	public Book() {
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String[] getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(String[] authors) {
+		this.authors = authors;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getPublishedYear() {
+		return publishedYear;
+	}
+
+	public void setPublishedYear(int publishedYear) {
+		this.publishedYear = publishedYear;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	
+	
+}
---
+++ RESTfulMicroservice/src/main/java/books/microservice/BooksMicroService.java
@@ -0,0 +1,8 @@
+package books.microservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksMicroService {
+
+}
