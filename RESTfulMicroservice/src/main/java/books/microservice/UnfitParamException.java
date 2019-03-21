a5b08b6aa9b56df2bf0fc63845e0c42556fb6385
Oren Shadmi <OrenShadmi@gmail.com>
2019-03-21 16:59:46 +0200

    Not Found

---
+++ RESTfulMicroservice/src/main/java/books/microservice/UnfitParamException.java
@@ -0,0 +1,26 @@
+package books.microservice;
+
+public class UnfitParamException extends Exception{
+
+	private static final long serialVersionUID = -822236855464414253L;
+
+	public UnfitParamException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UnfitParamException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UnfitParamException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UnfitParamException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
